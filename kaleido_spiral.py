35b78a50902125487a6bdd918fee748c4fea3446
Mathimurugan-Miththilan <134406906+Mathimurugan-Miththilan@users.noreply.github.com>
2023-06-19 20:46:53 +0530

    Add files via upload

--- kaleido_spiral.py
+++ kaleido_spiral.py
@@ -1,18 +1,15 @@
 import turtle as t
-
 from itertools import cycle
+
 colors = cycle(['red','orange','yellow','green','blue','purple'])
 
-def draw_circle(size,angle,shift):
-    t.bgcolor(next(colors))
+def draw_circle(size):
     t.pencolor(next(colors))
     t.circle(size)
-    t.right(angle)
-    t.forward(shift)
-    draw_circle(size+5,angle+1,shift+1)
+    draw_circle(size+5)
 
 t.bgcolor('black')
 t.speed('fast')
 t.pensize(4)
-draw_circle(30,0,1)
+draw_circle(30)
 
