c79b3663e548023cbc7e857360a5530bb6425ebd
Mathimurugan-Miththilan <134406906+Mathimurugan-Miththilan@users.noreply.github.com>
2023-06-19 21:04:39 +0530

    Add files via upload

--- kaleido_spiral.py
+++ kaleido_spiral.py
@@ -4,12 +4,11 @@ from itertools import cycle
 colors = cycle(['red','orange','yellow','green','blue','purple'])
 
 def draw_circle(size,angle,shift):
-    t.bgcolor(next(colors))
     t.pencolor(next(colors))
     t.circle(size)
     t.right(angle)
     t.forward(shift)
-    draw_circle(size+5,angle+1,shift+1)
+    draw_circle(size+10,angle+10,shift+1)
 
 t.bgcolor('black')
 t.speed('fast')
